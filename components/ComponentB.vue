5e4d700558ce9803115742a39d39cc7706411959
yichengjie <626659321@qq.com>
2016-07-02 15:39:18 +0800

    vue-loader学习demo

---
+++ components/ComponentB.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="example">这是一个测试程序</div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        msg: 'Hello world B!'
+      }
+    }
+  }
+</script>
