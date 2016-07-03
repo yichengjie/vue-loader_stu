8c5bc79e8ad56acc11282a0688f588c8c1dddeea
yichengjie <626659321@qq.com>
2016-07-03 12:09:16 +0800

    commit

--- src/scripts/components/App.vue
+++ src/scripts/components/App.vue
@@ -2,17 +2,20 @@
   <div class="app">
     <component-a></component-a>
     <component-b></component-b>
+    <header-comp></header-comp>
   </div>
 </template>
 
 <script>
-  import ComponentA from './ComponentA.vue'
-  import ComponentB from './ComponentB.vue'
+  import ComponentA from './ComponentA.vue' ;
+  import ComponentB from './ComponentB.vue' ;
+  import HeaderComp from './HeaderComp.vue' ;
 
   export default {
     components: {
       ComponentA,
-      ComponentB
+      ComponentB,
+      HeaderComp
     }
   }
 </script>
