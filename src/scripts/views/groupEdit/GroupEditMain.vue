722bd82b58b5a154ccbe26c1111ea061da63999c
yichengjie <626659321@qq.com>
2016-07-05 21:19:41 +0800

    validator加强

--- src/scripts/views/groupEdit/GroupEditMain.vue
+++ src/scripts/views/groupEdit/GroupEditMain.vue
@@ -3,13 +3,14 @@
 		<div class="col-sm-8">
 			<validator name="validation">
 			<form class="form-horizontal" novalidate>
-			  <div class="form-group" :class="{ 'has-error': $validation.email.minlength }">
+			  <div class="form-group" 
+			  	:class="{ 'has-error': $validation.email.minlength &&$validation.email.dirty}">
 			    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 			    <div class="col-sm-6">
 			      <input type="text" class="form-control" v-validate:email="{ minlength: 2, maxlength: 10 }"
 			      id="inputEmail3"  placeholder="Email">
 			    </div>
-			    <div class="col-sm-4 errors">
+			    <div class="col-sm-4 errors" v-if ="$validation.email.dirty">
 			    	 <span v-show="$validation.email.minlength">Your comment is too short.</span>
       				<span v-show="$validation.email.maxlength">Your comment is too long.</span>
 			    </div>
@@ -21,28 +22,33 @@
 			    </div>
 			  </div>
 			  <div class="form-group">
-			    <div class="col-sm-offset-2 col-sm-6">
-			      <div class="checkbox">
-			        <label>
-			          <input type="checkbox"> Remember me
-			        </label>
-			      </div>
-			    </div>
-			  </div>
-			  <div class="form-group">
 			    <div class="col-sm-offset-2 col-sm-10">
-			      <button type="button" class="btn btn-default">Sign in</button>
+			      <button type="button" class="btn btn-default" @click="submitForm">提交</button>
 			    </div>
 			  </div>
 			</form>
 			</validator>
 		</div>
+		<div class="cols-sm-4">
+			<pre>
+				{{$validation | json}}
+			</pre>
+		</div>
 	</div>
 </template>
-
+<script>
+  export default {
+    methods:{
+    	submitForm(){//点击提交按钮
+    		var validator = this.$validation ;
+			validator.email.dirty = true ;
+    	}
+    }
+  }
+</script>
 <style>
 	.errors{
-		color:red;
+		color:#a94442;
 		margin-top: 7px;
 	}
 </style>
